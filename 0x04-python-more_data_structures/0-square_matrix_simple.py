60cd9b2e88724790aaac34765601c0532176b5fb
Dare Omotayo <dareomotayo32@gmail.com>
2022-06-10 04:20:36 +0100

    Data structure is cool

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,15 @@
-# #!/usr/bin/python3
-# def square_matrix_simple(matrix=[]):
-#     new_matrix = matrix.copy()
+#!/usr/bin/python3
+ def square_matrix_simple(matrix=[]):
+     new_matrix = matrix.copy()
 
-#     for i in range(len(matrix)):
-#         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+     for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-#     return (new_matrix)
+        return (new_matrix)
 
-def square_matrix_simple(L):
-    if type(L) is list:
-        return [square_matrix_simple(x) for x in L]
-    else:
-        new_matrix = L**2
-        return new_matrix
+# def square_matrix_simple(L):
+#    if type(L) is list:
+#        return [square_matrix_simple(x) for x in L]
+#    else:
+#        new_matrix = L**2
+#        return new_matrix
