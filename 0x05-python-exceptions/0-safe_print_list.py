2f55853b9f2d63d572ae07891a5f66cb218f1313
Dare Omotayo <dareomotayo32@gmail.com>
2022-06-23 09:15:11 +0100

    Was working on the file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python3
-# def safe_print_list(my_list=[], x=0):
-#     count = 0
+!/usr/bin/python3
+ def safe_print_list(my_list=[], x=0):
+     count = 0
 
-#     for i in range(x):
-#         try:
-#             print("{}".format(my_list[i]), end='')
-#         except:
-#             break
-#         else:
-#             count += 1
+     for i in range(x):
+         try:
+             print("{}".format(my_list[i]), end='')
+         except:
+             break
+         else:
+             count += 1
 
-#     print()
-#     return (count)
+     print()
+     return (count)
 # my_list = [1, 2, 3, 4, 5,1,2,4,5,7,8]
 # safe_print_list(my_list, 6)
 # list= [1,2,3,4,5,6,7,8,9,10]
@@ -21,6 +21,6 @@
 # prices = [10,20,30]
 # for item in prices:
 #     total += item
-numbers = [2, 2, 2, 2, 6]
-for i in numbers:
-    print(f"X" * i)
+#numbers = [2, 2, 2, 2, 6]
+#for i in numbers:
+#    print(f"X" * i)
