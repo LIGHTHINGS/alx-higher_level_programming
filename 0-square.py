b3c8a860c19ad4e834650ae7b517566c62adee5d
Dare Omotayo <dareomotayo32@gmail.com>
2022-10-04 12:38:20 +0100

    Square Class

---
+++ 0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class
+"""
+
+class Square:
+    """
+    This is the Skeleton(squeleton) for Square class
+    """
+    pass
---
+++ 1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class
+"""
+
+class Square:
+    """Square class implementation
+    """
+    def __init__(self, size):
+        self.__size = size
